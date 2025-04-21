ef70ab4182c813e929309c8196e9c3e99691a3eb
Vinitshirpurkar <vinitshirpurkar123@gmail.com>
2025-04-21 15:13:42 +0530

    added new

--- EC2/Ec2.py
+++ EC2/Ec2.py
@@ -1,6 +1,7 @@
 import boto3
 
 ec2_resource = boto3.resource('ec2')
+ec2_client = boto3.client('ec2')
 """ ec2_resource.create_instances(ImageId='ami-0e449927258d45bc4',
                                InstanceType='t2.micro',
                                MaxCount=1,
@@ -8,8 +9,15 @@ ec2_resource = boto3.resource('ec2')
 
 print("created") """
 
-ec2_resource.create_key_pair(
+""" ec2_resource.create_key_pair(
     KeyName = "my_key_pair",
     KeyFormat = 'pem'
+) """
+result = ec2_client.describe_instances(
+    InstanceIds = ["i-0c1e1c8c866078afc"]
 )
 
+""" print(result['Reservations'][0]) """
+
+ec2_client.stop_instances(InstanceIds = ["i-0c1e1c8c866078afc"])
+print("instance stopped")
